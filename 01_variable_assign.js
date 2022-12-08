5b60c0995f0df01829485160533266026f98fd5e
Pallav2011 <ranpisepg09@gmail.com>
2022-12-08 16:54:52 +0530

    created 01_variable_assign.js
    created 01_variable_assign.js
    for demonstrating variable declaration and intialisation.

---
+++ 01_variable_assign.js
@@ -0,0 +1,17 @@
+var myFullName;
+console.log(myFullName);
+myFullName = "Pandurang Ranpise";
+console.log(myFullName);
+myFullName = "Pandurang Gopal Ranpise";
+console.log(myFullName);
+
+var myLove = "I love only JS";
+console.log(myLove);
+var firstName = "Pandurang",
+  lastName = "Ranpise",
+  age = "24",
+  adress = "Pandharpur",
+  pin_code = "413304",
+  gender = "Male",
+  is_married = "Unmarried";
+console.log(firstName, lastName, age, adress, pin_code, gender, is_married);
