f351cf1412df37ae30907cbe5eda9a3454603565
Pallav2011 <ranpisepg09@gmail.com>
2022-12-09 17:02:26 +0530

    variable and datatypes assignment
    variable and datatypes assignment.

---
+++ 01_typeOf.js
@@ -0,0 +1,25 @@
+var bankName;
+console.log("***** Before Initialisation *****");
+console.log("bankName=",bankName);
+console.log("Datatype=", typeof(bankName));
+bankName="SBI Bank";
+console.log("***** After Initialisation *****");
+console.log("bankName=",bankName);
+console.log("Datatype=", typeof(bankName));
+
+var marks='90%';
+console.log("\nDatatype of marks=",typeof(marks));
+
+var isWorking;
+console.log("\n***** Before Initialisation *****");
+console.log("value of isWorking=",isWorking);
+isWorking=true;
+console.log("***** After Initialisation *****");
+console.log("value of isWorking=",isWorking);
+console.log("Datatype of isWorking=",typeof(isWorking));
+
+var totalCount="Hundred and Seven";
+console.log("");
+console.log(typeof(totalCount),totalCount);
+totalCount=107;
+console.log(typeof(totalCount),totalCount);
