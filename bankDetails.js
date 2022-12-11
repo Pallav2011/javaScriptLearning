126aa522ecee45c29378c27d33b1357a87360d7f
Pallav2011 <ranpisepg09@gmail.com>
2022-12-11 17:32:03 +0530

    created bankDetails.js
    created bankDetails.js .function assignment

---
+++ bankDetails.js
@@ -0,0 +1,7 @@
+function bankDetails(bankName,accountNum,location,pinCode) {
+    console.log("Bank name:",bankName,',',"Account number:",accountNum,',',"Location:",location,',',"Pin code:",pinCode);
+}
+console.log("Following are the Bank details:");
+bankDetails("CITI Bank",3456782345,'pune',431202);
+bankDetails("Axis Bank",7856782345,'Mumbai',441202);
+bankDetails("HDFC Bank",8956782345,'pune',631202);
