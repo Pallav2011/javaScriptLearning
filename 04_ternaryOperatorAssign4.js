bad0ba60b562acf8d8a639d766ea7c791a25a35f
Pallav2011 <ranpisepg09@gmail.com>
2022-12-16 18:05:43 +0530

    EligibilityTCS check using ternary operator
    EligibilityTCS check using ternary operator

---
+++ 04_ternaryOperatorAssign4.js
@@ -0,0 +1,8 @@
+var eligibilityTCS = function (gradScore,hscScore,sscScore,candName){
+var res = gradScore>=70 || hscScore>=80 ||sscScore>=90 ? `Congrats ${candName} you are eligible for interview`
+:`unfortunatly ${candName} you are not eligible for interview`;
+console.log(res);
+}
+eligibilityTCS(80,86,90,"Pandurang");
+eligibilityTCS(70,65,55,"Rahul");
+eligibilityTCS(60,79,88,"Nitin");
