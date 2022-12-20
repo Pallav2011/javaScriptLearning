8974c5c03ab2661629860256701d2fa1fc7fbdf3
Pallav2011 <ranpisepg09@gmail.com>
2022-12-20 16:54:18 +0530

    created forLoopAssignment.js
    created forLoopAssignment.js

---
+++ 06_forLoopAssign.js
@@ -0,0 +1,34 @@
+console.log(`1) WAP to print numbers from 5 to 15 by increament by 1`);
+for (let index = 5; index <=15; index++){
+    console.log(index);
+}
+console.log(`\n2) WAP to print numbers from 50 to 40 by decreament by 1`);
+for (let index = 50; index >=40; index--){
+    console.log(index);
+}
+console.log(`\n3) WAP to find first 15 Odd numbers`);
+for (let index = 0; index <=30; index++){
+    if(index%2==0){
+
+    }else{
+        console.log(index);
+    }
+}
+console.log(`\n4) WAP to find first 10 Even numbers`);
+for (let index = 0; index <=20; index++){
+    if(index%2==0){
+        console.log(index);
+    }else{}  
+}
+console.log(`\n5) WAP to print table of 5`);
+for (let index = 5; index <=50; index=index+5){
+    console.log(index);
+}
+console.log(`\n6) WAP to print table of 10`);
+for (let index = 10; index <=100; index=index+10){
+    console.log(index);
+}
+console.log(`\n7) WAP to print table of 10 in reverse order`);
+for (let index = 100; index >=10; index=index-10){
+    console.log(index);
+}
