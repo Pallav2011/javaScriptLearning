2763955f0d23cc107040c423437f7125d730b8f7
Pallav2011 <ranpisepg09@gmail.com>
2022-12-21 18:00:20 +0530

    created whileLoopAndDoWhileLoopAssignmant.js
    created whileLoopAndDoWhileLoopAssignmant.js

---
+++ 06_whileLoopAndDoWhileLoopAssign.js
@@ -0,0 +1,50 @@
+console.log(`1)WAP to print numbers from 5 to 15 by increamenting 1 using while loop`);
+var i=5;
+while (i<=15) {
+    console.log(i);
+    i++;
+}
+console.log(`2)WAP to print numbers from 50 to 40 by decreamenting 1 using while loop`);
+var i2=50;
+while (i2>=40) {
+    console.log(i2);
+    i2--;
+}
+console.log(`3)WAP to find first 15 Odd numbers using while loop`);
+var i3=0;
+while (i3<=30) {
+    if(i3%2==0){
+
+    }else{
+        console.log(i3);
+    }
+        i3++;
+}
+console.log(`4)WAP to find first 10 Even numbers using while loop`);
+var i4=0;
+while (i4<=20) {
+    if(i4%2==0){
+        console.log(i4);
+    }else{
+        
+    }
+        i4++;
+}
+console.log(`5)WAP to print table of 5 using do while loop`);
+var i5=5;
+do {
+    console.log(i5);
+    i5=i5+5;
+} while (i5<=50);
+console.log(`6)WAP to print table of 10 using do while loop`);
+var i6=10;
+do {
+    console.log(i6);
+    i6=i6+10;
+} while (i6<=100);
+console.log(`7)WAP to print table of 10 in reverse order using do while loop`);
+var i7=100;
+do {
+    console.log(i7);
+    i7=i7-10;
+} while (i7>=10);
