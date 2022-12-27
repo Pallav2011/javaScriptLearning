dc5b33c4e775ec5c54835326c7701970ab1a945b
Pallav2011 <ranpisepg09@gmail.com>
2022-12-27 20:09:43 +0530

    update arrayMethodAssignment2.js
    update arrayMethodAssignment2.js

--- 07_arrayMethodsAssign2.js
+++ 07_arrayMethodsAssign2.js
@@ -1,11 +1,14 @@
 const arrayNumbers=[20,31,40,25,23,11,29,9,60,2,11];
 console.log(`============1) Find the total elements and length================`);
 console.log(arrayNumbers);
+
 console.log(`============2)First element and Last element of array================`);
 console.log(`First element of array is : ${arrayNumbers[0]}`);
 console.log(`Last element of array is : ${arrayNumbers[arrayNumbers.length-1]}`);
+
 console.log(`============3)Thirst last element of array================`);
 console.log(`Thirst last element is : ${arrayNumbers[arrayNumbers.length-3]}`);
+
 console.log(`============4)Find the all even numbers from array================`);
 let evenNumbers="";
 for (const element of arrayNumbers) {
@@ -14,6 +17,7 @@ for (const element of arrayNumbers) {
     }
 }
 console.log(evenNumbers);
+
 console.log(`============5)Find the all odd numbers from array================`);
 let oddNumbers="";
 for (const element of arrayNumbers) {
@@ -22,6 +26,7 @@ for (const element of arrayNumbers) {
     }
 }
 console.log(oddNumbers);
+
 console.log(`============6)Find the even positioned elements from array================`);
 let evenPositionedNumbers="";
 for (let index = 0; index < arrayNumbers.length; index++) {
@@ -30,6 +35,7 @@ for (let index = 0; index < arrayNumbers.length; index++) {
    }
 }
 console.log(evenPositionedNumbers);
+
 console.log(`============7)Find the odd positioned elements from array================`);
 let oddPositionedNumbers="";
 for (let index = 0; index < arrayNumbers.length; index++) {
@@ -38,12 +44,14 @@ for (let index = 0; index < arrayNumbers.length; index++) {
    }
 }
 console.log(oddPositionedNumbers);
+
 console.log(`============8)Find the sum of all elements of array================`);
 let sumOfArrayNumber=0;
 for (const element of arrayNumbers) {
     sumOfArrayNumber=sumOfArrayNumber+element;
 }
 console.log(`Sum of array numbers is : ${sumOfArrayNumber}`);
+
 console.log(`============9)Find the number which are multiple of 5================`);
 let multipleOfFiveNumbers="";
 for (const element of arrayNumbers) {
@@ -52,15 +60,19 @@ for (const element of arrayNumbers) {
     }
 }
 console.log(multipleOfFiveNumbers);
+
 console.log(`============10)is number 115 available in array================`);
 let isAvailable115=arrayNumbers.includes(115);
 console.log(`is number 115 available in array : ${isAvailable115}`);
+
 console.log(`============11)is number 23 available in array================`);
 let isAvailable23=arrayNumbers.includes(23);
 console.log(`is number 23 available in array : ${isAvailable23}`);
+
 console.log(`============12)insert number 55, 66 before index 3 ================`);
 let insertNumbers=arrayNumbers.splice(3 , 0 , 55,66);
 console.log(arrayNumbers);
+
 console.log(`============13)delete 3 elements from index 4 ================`);
 let deleteElements=arrayNumbers.splice(4 , 3);
 console.log(arrayNumbers);
