13bc86856f1952bea113d243aaaa999a463bdf0b
Pallav2011 <ranpisepg09@gmail.com>
2023-01-25 15:28:46 +0530

    update sortingOnObject.js
    update sortingOnObject.js

--- 14_sortingOnObject.js
+++ 14_sortingOnObject.js
@@ -26,3 +26,4 @@ console.log(`-------------------------------------------------------------------
 console.log(`3) sorting array of employees in ascending order by Salary:`);
 const sortedArrayBySalary = arraEmployees.sort((id1,id2)=> id1.empSalary>id2.empSalary ? -1 : 1 );
 sortedArrayBySalary.forEach(val=>console.log(`Employee Name is: ${val.empName}, Salary: ${val.empSalary} and Company: ${val.empCompany}`));
+let num1= console.log(`Enter the Value:`);
